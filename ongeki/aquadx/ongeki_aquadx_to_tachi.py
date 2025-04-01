e8af14132f86f086699403a86c35bee4e7f0ac03
Pinapelz <yukais@pinapelz.com>
2025-04-01 16:55:40 -0700

    aquadx_ongeki: add pulling ongeki playdata using token from AquaNet

--- ongeki/aquadx/ongeki_aquadx_to_tachi.py
+++ ongeki/aquadx/ongeki_aquadx_to_tachi.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import urllib.request
+import os
 
 ONGEKI_AQUADX_JSON = "https://aquadx.net/d/ongeki/00/all-music.json"
 
@@ -16,17 +17,7 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
 
-def convert_from_aquadx_json_to_tachi_json(input_json: str, output_file: str, service: str, music_json: str):
-    if music_json == "online":
-        req = urllib.request.Request(ONGEKI_AQUADX_JSON, headers=headers)
-        with urllib.request.urlopen(req) as response:
-            music_json = json.load(response)
-    else:
-        with open(music_json, "r", encoding="utf-8") as file:
-            music_json = json.load(file)
-
-    with open(input_json, "r", encoding="utf-8") as f:
-        raw_data = json.load(f)
+def convert_from_aquadx_json_to_tachi_json(raw_data: str, output_file: str, service: str, music_json: str):
 
     batch_manual = {
         "meta": {"game": "ongeki", "playtype": "Single", "service": service},
@@ -112,16 +103,48 @@ if __name__ == "__main__":
         description="Converts AquaDX API JSON for O.N.G.E.K.I to Tachi compatible JSON",
         epilog="damage, fast, slow, unavailable on the webui"
     )
-    parser.add_argument("input_file", help="Path to the input JSON file exported from AquaDX")
+    parser.add_argument("-f", "--file", help="Manual. Specify path to the input exported score JSON file exported from AquaDX", required=False)
     parser.add_argument(
         "-s",
         "--service",
         help="Service description to be shown on Tachi (Note for where this score came from)",
-        default="AquaDX O.N.G.E.K.I Import (API JSON)",
+        default="AquaDX Chuni Import",
     )
+    parser.add_argument("-t", "--token", help="Use AquaNet Token to directly grab data from API. Get it from Network tab in your browser and check the API request it makes ?token=???", required=False)
+    parser.add_argument("-u", "--url", help="AquaNet API endpoint. No need to use this unless you self-host AquaDX", default="https://aquadx.net/aqua")
     parser.add_argument(
-        "-o", "--output", help="Output filename", default="aquadx_ongeki_tachi.json"
+        "-o", "--output", help="Output filename", default="aquadx_mai2_tachi.json"
     )
-    parser.add_argument("--music", "--music-file", help="JSON file containing the mappings of song names to IDs. Check README for moe info", default="online")
+    parser.add_argument("-m", "--music", help="all-music.json from AquaNet that maps song id to name (required for Tachi). It will automatically pull from main AquaDX if not specified", default="online")
     args = parser.parse_args()
-    convert_from_aquadx_json_to_tachi_json(args.input_file, args.output, args.service, args.music)
+    # Some checks to make sure input is valid
+    if args.token is None and args.file is None:
+        print("ERROR: No valid input method specified. You must specify either --file or --token")
+        exit(1)
+    aquadx_url = args.url
+    if not aquadx_url.startswith("https://") and not aquadx_url.startswith("http://"):
+        aquadx_url = "https://" + aquadx_url
+
+
+    if args.file is not None:
+        print("An input file has been specified, using local file as input")
+        if not os.path.exists(args.file):
+            print(f"ERROR: The file {args.file} does not exist.")
+            exit(1)
+        with open(args.file, "r", encoding="utf-8") as f:
+            raw_data = json.load(f)
+    else:
+        print("Pulling mu3/ONGEKI playdata from remote AquaDX at: " + aquadx_url)
+        req = urllib.request.Request(aquadx_url+"/api/v2/game/ongeki/user-summary?username=pinapelz&token="+args.token, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            raw_data = json.load(response)
+
+    if args.music == "online":
+        req = urllib.request.Request(ONGEKI_AQUADX_JSON, headers=headers)
+        with urllib.request.urlopen(req) as response:
+            music_json = json.load(response)
+    else:
+        with open(args.music, "r", encoding="utf-8") as file:
+            music_json = json.load(file)
+    args = parser.parse_args()
+    convert_from_aquadx_json_to_tachi_json(raw_data, args.output, args.service, music_json)
